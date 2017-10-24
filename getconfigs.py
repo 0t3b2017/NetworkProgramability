7170ef5aaa01e7a0ad0df9fca437f6e82b5d7a46
roberto <roberto@h3b.com.br>
2017-10-24 11:53:31 -0200

    Add: Paramiko example 1 Add: Script to add int in vlan (brocade)

--- getconfigs.py
+++ getconfigs.py
@@ -1,36 +1,42 @@
 #!/usr/bin/env python
 
-## Python 2.7
-import getpass
-import sys
-import telnetlib
+# Python 2.7
 
-# Ask for username and password
-user = raw_input("Enter your remote account: ")
-password = getpass.getpass()
+import paramiko
+import time
+
+username = "roberto"
+password = "cisco"
+
 
 # Open a file called myswitches (with the IP addresses to connect to)
 f = open('myswitches')
 
-# Telnet to hosts and get the running config
+# Create a loop to connect to all devices on 'myswitches' file and configure them.
+
 for line in f:
-    HOST = line.strip()
-    print("Getting running-config from host {}".format(HOST))
-    tn = telnetlib.Telnet(HOST)
+    if line[0] != "#":
+        ip_address = line.strip()
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(hostname=ip_address, username=username, password=password)
 
-    tn.read_until("Username: ")
-    tn.write(user + "\n")
-    if password:
-        tn.read_until("Password: ")
-        tn.write(password + "\n")
+        print("Successful connection to ", ip_address)
 
-    tn.write("terminal length 0\n")
-    tn.write("show run\n")
-    tn.write("exit\n")
+        client = ssh_client.invoke_shell()
 
-    readoutput = tn.read_all()
-    saveoutput = open("switch" + HOST, "w")
-    saveoutput.write(readoutput)
-    saveoutput.close
+        print("Getting running-config from host {}".format(ip_address))
 
-    print tn.read_all()
+        client.send("terminal length 0\n")
+        time.sleep(1)
+        client.send("show run\n")
+        time.sleep(5)
+        client.send("exit\n")
+  
+        output = client.recv(65535)
+        print output
+
+        saveoutput = open("bkp_switch" + ip_address, "w")
+        saveoutput.write(output)
+
+        ssh_client.close()
--- paramiko_broc_int_add_vlan.py
+++ paramiko_broc_int_add_vlan.py
@@ -1,9 +1,26 @@
+#!/usr/bin/env python
+
+"""
+This is a script used to add interfaces tagged to some VLANs.
+Used in Brocade switches
+by: 0T3B
+"""
+
 import paramiko
 import time
 
 ip_address = "192.168.122.253"
 username = "roberto"
 password = "cisco"
+interface = '10/18'
+
+"""
+vlan_first = int(input("Enter the first VLAN: "))
+vlan_last = int(input("Enter the last VLAN: "))
+"""
+
+vlan_first = 100
+vlan_last = 1049
 
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -14,24 +31,19 @@ print "Successful connection", ip_address
 client = ssh_client.invoke_shell()
 
 client.send("configure terminal\n")
-client.send("int loop 0\n")
-client.send("ip address 1.1.1.1 255.255.255.255\n")
-client.send("int loop 3\n")
-client.send("ip address 3.3.3.3 255.255.255.255\n")
-client.send("router ospf 1\n")
-client.send("network 0.0.0.0 255.255.255.255 area 0\n")
 
-for n in range(2, 21):
-    print "Creating VLAN " + str(n)
+for n in range(vlan_first, vlan_last + 1):
+    print "Adding VLAN " + str(n)
     client.send("vlan " + str(n) + "\n")
-    client.send("name Python_VLAN " + str(n) + "\n")
-    time.sleep(0.5)
+    client.send("tagged ethernet " + str(interface) + "\n")
+    time.sleep(2)
 
 client.send("end\n")
+
+client.send("wr mem\n")
 
 time.sleep(1)
 output = client.recv(65535)
 print output
-# print type(output)
 
 ssh_client.close()
